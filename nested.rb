562991a41e7c777c85ce0fd4b45c55955352c07d
Matthew Malecki <mmalecki4@gmail.com>
2020-04-16 22:26:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
     }
   programmer_hash[:yukihiro_matsumoto] = Hash.new
     programmer_hash[:yukihiro_matsumoto][:known_for]
-      programmer_hash[:yukihiro_matsumoto][:known_for]
+      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages]
-      programmer_hash[:yukihiro_matsumoto][:languages]
+      programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
 end
 
 def changing_alan
